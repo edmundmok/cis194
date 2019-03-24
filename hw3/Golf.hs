5b2213d5a974691695c04058ead140723927518f
Edmund Mok <edmundmok@outlook.com>
2019-03-24 19:59:25 +0800

    Add hw3/Golf.hs::localMaxima

--- hw3/Golf.hs
+++ hw3/Golf.hs
@@ -3,4 +3,14 @@
 module Golf where
 
 skips :: [a] -> [[a]]
-skips xs = [map fst (filter (\(_, y) -> (y `mod` n == 0)) (zip xs [1..]))  | n <- [1..length xs]]
+skips xs = [map fst (filter (\(_, y) -> (y `mod` n == 0)) (zip xs [1..])) | n <- [1..length xs]]
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima xs
+  | length xs < 3 = []
+  | otherwise =
+    let
+      ys = tail xs
+      zs = tail ys
+    in
+      foldr (\(x, y, z) l -> if (y > x && y > z) then (y : l) else l) [] (zip3 xs ys zs)
