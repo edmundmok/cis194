3b5fc6953ea4bfb2f49ba3929290bf8edf5b2c69
Edmund Mok <edmundmok@outlook.com>
2019-03-24 21:24:39 +0800

    Add hw3/Golf.hs::histogram

--- hw3/Golf.hs
+++ hw3/Golf.hs
@@ -2,15 +2,19 @@
 
 module Golf where
 
+import Data.List
+
 skips :: [a] -> [[a]]
 skips xs = [map fst (filter (\(_, y) -> (y `mod` n == 0)) (zip xs [1..])) | n <- [1..length xs]]
 
 localMaxima :: [Integer] -> [Integer]
 localMaxima xs
   | length xs < 3 = []
-  | otherwise =
-    let
-      ys = tail xs
-      zs = tail ys
-    in
-      foldr (\(x, y, z) l -> if (y > x && y > z) then (y : l) else l) [] (zip3 xs ys zs)
+  | otherwise = foldr (\(x, y, z) l -> if (y > x && y > z) then (y : l) else l) [] (zip3 xs (tail xs) (tail $ tail xs))
+
+histogram :: [Integer] -> String
+histogram xs =
+  let
+    ys = map (\zs -> (length zs)-1) (group $ sort (filter (\x -> x >= 0 && x <= 9) xs ++ [0..9]))
+  in
+    unlines [concat [if (ys !! c > r) then "*" else " " | c <- [0..9]] | r <- reverse [0..(maximum ys)-1]] ++ "==========\n0123456789\n"
