4a858cb55e4fa192450d15c40d71c38ef41c61c0
Edmund Mok <edmundmok@outlook.com>
2019-03-28 23:52:19 +0800

    Handle foldTree

--- hw4/Folds.hs
+++ hw4/Folds.hs
@@ -26,6 +26,18 @@ data Tree a = Leaf
             | Node Integer (Tree a) a (Tree a)
   deriving (Show, Eq)
 
+foldTree :: [a] -> Tree a
+foldTree = foldr insertIntoTree Leaf
+
+insertIntoTree :: a -> Tree a -> Tree a
+insertIntoTree v Leaf = Node 0 Leaf v Leaf
+insertIntoTree v (Node h Leaf v' r) = Node (h+1) (insertIntoTree v Leaf) v' r
+insertIntoTree v (Node h l v' Leaf) = Node h l v' (insertIntoTree v Leaf)
+insertIntoTree v (Node _ l@(Node h' _ _ _) v' r@(Node h'' _ _ _))
+  | h' < h'' = let l'@(Node hh' _ _ _) = insertIntoTree v l
+                in Node ((+1) $ max hh' h'') l' v' r
+  | otherwise = let r'@(Node hh'' _ _ _) = insertIntoTree v r
+                in Node ((+1) $ max h' hh'') l v' r'
 xor :: [Bool] -> Bool
 xor = odd . foldl' (\x y -> x + fromEnum y) 0
 
