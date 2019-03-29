2ef534988069b139520c0466fa77ae916bbae23b
Edmund Mok <edmundmok@outlook.com>
2019-03-29 19:26:31 +0800

    Add evalStr

--- hw5/Calc.hs
+++ hw5/Calc.hs
@@ -2,10 +2,15 @@
 
 module Calc where
 
+import Control.Monad
 import ExprT
+import Parser
 
 -- Exercise 1
 eval :: ExprT -> Integer
 eval (Lit v) = v
 eval (Add l r) = (eval l) + (eval r)
 eval (Mul l r) = (eval l) * (eval r)
+
+evalStr :: String -> Maybe Integer
+evalStr = parseExp Lit Add Mul >=> (\x -> return (eval x))
