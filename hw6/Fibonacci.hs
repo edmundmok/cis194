fd774996777c138cb7fc3157f1ef3e03842c13eb
Edmund Mok <edmundmok@outlook.com>
2019-04-06 16:23:34 +0800

    Add binary exponentiation version of fib

--- hw6/Fibonacci.hs
+++ hw6/Fibonacci.hs
@@ -76,3 +76,16 @@ instance Fractional (Stream Integer) where
 
 fibs3 :: Stream Integer
 fibs3 = x / (1 - x - (x * x))
+
+-- Exercise 7
+data Matrix = Matrix Integer Integer Integer Integer
+  deriving (Show)
+
+instance Num Matrix where
+  (*) (Matrix a1 a2 a3 a4) (Matrix b1 b2 b3 b4) = Matrix (a1*b1 + a2*b3) (a1*b2 + a2*b4) (a3*b1 + a4*b3) (a3*b2 + a4*b4)
+
+fib4 :: Integer -> Integer
+fib4 n
+  | n == 0 = 0
+  | otherwise = s
+      where (Matrix _ s _ _) = (Matrix 1 1 1 0) ^ n
