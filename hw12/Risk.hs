fbca0fc78ac83a5e05673cb78933d0b9aeba7227
Edmund Mok <edmundmok@outlook.com>
2019-05-10 12:49:12 +0800

    Add implementation for Risk

--- hw12/Risk.hs
+++ hw12/Risk.hs
@@ -3,6 +3,7 @@
 module Risk where
 
 import Control.Monad.Random
+import Data.List
 
 ------------------------------------------------------------
 -- Die values
@@ -25,4 +26,28 @@ die = getRandom
 
 type Army = Int
 
-data Battlefield = Battlefield { attackers :: Army, defenders :: Army }
+data Battlefield = Battlefield { attackers :: Army, defenders :: Army } deriving (Show)
+
+battle :: Battlefield -> Rand StdGen Battlefield
+battle (Battlefield atkSize defSize) = do
+  let atk = (min 3 . flip (-) 1) atkSize
+      def = min 2 defSize
+  atkRolls <- replicateM atk die
+  defRolls <- replicateM def die
+  let sAtkRolls = (reverse . sort) atkRolls
+      sDefRolls = (reverse . sort) defRolls
+      sRolls = zip sAtkRolls sDefRolls
+      defLosses = (foldl' (+) 0 . map (fromEnum . uncurry (>))) sRolls
+      atkLosses = (length sRolls) - defLosses
+  return (Battlefield (atkSize - atkLosses) (defSize - defLosses))
+
+invade :: Battlefield -> Rand StdGen Battlefield
+invade b@(Battlefield atkSize defSize)
+  | atkSize < 2 || defSize  == 0 = return b
+  | otherwise = (battle b) >>= invade
+
+successProb :: Battlefield -> Rand StdGen Double
+successProb b = do
+  results <- (replicateM 1000 . invade) b
+  let successes = (foldl1' (+) . map (fromEnum . (== 0) . defenders)) results
+  return $ (fromIntegral successes) / 1000
